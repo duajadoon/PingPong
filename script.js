aa6e05664ba8dbff53df20227dbcd09351449e61
duajadoon <38002659+duajadoon@users.noreply.github.com>
2018-04-23 19:02:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ var pingPong = function(number) {
 
     for (i = 1; i <= myNumber; i++)
       if (i % 3 === 0 && i % 5 === 0) {
-        $("ul").append("<li><img src ='d.png' width='20'>  <img src ='b.png' width='20'></li>");
+        $("ul").append("<li><img src ='a.png' width='20'>  <img src ='b.png' width='20'></li>");
 
       } else if (i % 3 === 0) {
-      $("ul").append("<li> <img src ='d.png' width='20'></li>");
+      $("ul").append("<li> <img src ='a.png' width='20'></li>");
 
     } else if (i % 5 === 0) {
       $("ul").append("<li> <img src ='b.png' width='20'></li>");
